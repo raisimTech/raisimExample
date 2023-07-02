33c64994a2fd56b6ba0e8c462f1b12f1b1cedfcb
Jemin Hwangbo <jhwangbo@kaist.ac.kr>
2023-07-02 14:11:49 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]) {
   auto binaryPath = raisim::Path::setFromArgv(argv[0]);
-  
+
   raisim::World world;
   auto laikago = world.addArticulatedSystem(binaryPath.getDirectory() + "\\rsc\\laikago\\laikago.urdf");
   auto ball = world.addSphere(0.1, 1);
@@ -15,10 +15,9 @@ int main(int argc, char* argv[]) {
   server.launchServer();
 
   for (int i=0; i<10000; i++) {
-    raisim::MSLEEP(2);
+    RS_TIMED_LOOP(world.getTimeStep()*1e6);
     server.integrateWorldThreadSafe();
   }
 
   server.killServer();
-  std::cout<<"end of simulation"<<std::endl;
 }
